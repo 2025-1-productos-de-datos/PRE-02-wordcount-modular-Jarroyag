42d38c714456b06fc19ab44a4ebffbf90dfd57fd
Jorge Arroyave <joarroyaveg@unal.edu.co>
2025-05-03 13:06:03 -0500

    version8

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -25,25 +25,7 @@ def main():
     counter = count_words(words,output_folder)
     write_count_words(counter, output_folder)
 
-    # count the frequency of the words in the files in the input directory
-    # counter = {}
-    # for filename in input_file_list:
-    #     with open("data/input/" + filename) as f:
-    #         for l in f:
-    #             for w in l.split():
-    #                 w = w.lower().strip(",.!?")
-    #                 counter[w] = counter.get(w, 0) + 1
-
-    ##
-    write_count_words(counter)
-
-def read_all_lines():
-    all_lines = read_all_lines()
-    return all_lines
-
-def new_func(all_lines):
-    all_lines = [line.lower().strip() for line in all_lines]
-    return all_lines
+    
 
 
 if __name__ == "__main__":
