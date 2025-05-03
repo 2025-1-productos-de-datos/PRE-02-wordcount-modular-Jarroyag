8c45ed0a387759d9c31e80a9b2f4088c091d9c07
Jorge Arroyave <joarroyaveg@unal.edu.co>
2025-05-03 11:57:24 -0500

    homework

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,7 +1,9 @@
  #obtain a list of files in the input directory
+import sys
 
+from internals.src._internals.count_words import count_words
 from internals.src._internals.preprocess_lines import preprocess_lines
-from internals.src._internals.split_in_words import split_in_words
+from internals.src._internals.split_into_words import split_into_words
 from internals.src.read_all_lines import read_all_lines
 
 from .internals.write_count_words import write_count_words
@@ -10,9 +12,17 @@ from .internals.write_count_words import write_count_words
 def main():
 
     
+    if len(sys.argv) != 3:
+        print("Usage: python3 -m homework <input_folder> <output_folder>")
+        sys.exit(1)
+
+    input_folder = "sys.argv[1]"
+    output_folder = "sys.argv[2]"
+
+    
     all_lines = read_all_lines(input_folder)
     all_lines = preprocess_lines(all_lines)
-    words = split_in_words(all_lines)
+    words = split_into_words(all_lines)
     counter = count_words(words,output_folder)
     write_count_words(counter, output_folder)
 
@@ -31,12 +41,6 @@ def main():
 def read_all_lines():
     all_lines = read_all_lines()
     return all_lines
-
-def count_words(words):
-    counter = {}
-    for word in words:
-        counter[word] = counter.get(word, 0) + 1
-    return counter
 
 def new_func(all_lines):
     all_lines = [line.lower().strip() for line in all_lines]
