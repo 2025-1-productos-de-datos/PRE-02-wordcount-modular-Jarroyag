15544f7ee23a71a76622cad932f6a89238a0e457
Jorge Arroyave <joarroyaveg@unal.edu.co>
2025-05-04 11:08:45 -0500

    corrección de la pabra strip en prepocess

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,4 +1,4 @@
- #obtain a list of files in the input directory
+ # obtain a list of files in the input directory
 import sys
 
 from ._internals.count_words import count_words
@@ -10,23 +10,19 @@ from ._internals.write_word_counts import write_word_counts
 
 def main():
 
-
-    
- if len(sys.argv) != 3:
+    if len(sys.argv) != 3:
         print("Usage: python3 -m homework <input_folder> <output_folder>")
         sys.exit(1)
 
- input_folder = sys.argv[1]
- output_folder = sys.argv[2]
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
 
-    
- all_lines = read_all_lines(input_folder)
- all_lines = preprocess_lines(all_lines)
- words = split_into_words(all_lines)
- counter = count_words(words)
- write_word_counts(counter, output_folder)
+    all_lines = read_all_lines(input_folder)
+    all_lines = preprocess_lines(all_lines)
+    words = split_into_words(all_lines)
+    counter = count_words(words)
+    write_word_counts(counter, output_folder)
 
-    
 
 if __name__ == "__main__":
     main()
